d3f4770e07af599bd1d37abdf2cf017cd07b998a
Imran Fernando <imran.fernando@petvisor.com>
2025-08-02 18:41:13 +0530

    Complete vue 2 WC

--- app-vue-2/vue.config.js
+++ app-vue-2/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    // config.entryPoints.clear(); // Clear all entry points
+
+    config.entry('vetstoria-widget').add('./src/vetstoria-widget.js');
+
+    config.output.filename('js/[name].js');
+
+    config.optimization.delete('splitChunks');
+  }
 })
