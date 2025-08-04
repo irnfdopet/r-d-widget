43be719e50526899596a2ddb780ef98ed8a0f386
Imran Fernando <imran.fernando@petvisor.com>
2025-08-04 14:36:18 +0530

    Add stencil js

---
+++ stencil/src/components/my-component/my-component.tsx
@@ -0,0 +1,33 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <section><h1>Web Component</h1>
+      <h2>Widget Code</h2><div>Hello, World! I'm {this.getText()}</div></section>;
+  }
+}
